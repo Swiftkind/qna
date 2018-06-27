26e9934d9cb0fc10491c8014021123110e84ee06
Anton Sison <antoninakisison@gmail.com>
2018-06-27 15:16:56 +0800

    Refactor code

--- users/serializers.py
+++ users/serializers.py
@@ -8,27 +8,21 @@ class UserAuthSerializer(serializers.Serializer):
         label='Password',
         style={'input_type': 'password'},
         trim_whitespace=False,
+        write_only=True
     )
+    user = None
 
     def validate(self, data):
         email = data.get('email')
         password = data.get('password')
 
-        if email and password:
-            user_qs = User.objects.filter(email=email)
+        users = User.objects.filter(email=email)
+        user = users.first()
+        if not users or not user.check_password(password):
+            #removed  or not user.is_active in codition.. user is inactive by default
+            raise serializers.ValidationError("Email/Password is incorrect. Please try again.")
 
-            if user_qs.count()==1:
-                user = user_qs.first()
-            else:
-                raise serializers.ValidationError("This User Does Not Exist")
-
-            if not user.check_password(password):
-                raise serializers.ValidationError("Incorrect Password")
-
-            if not user.is_active:
-                raise serializers.ValidationError("This user is no longer active")
-
-        data['user'] = user
+        self.user = user
         return data
 
 
